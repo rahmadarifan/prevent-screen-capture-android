dcbe187e8f6c8a6c8e94b71c014f85eeff7e9ef3
Rahmad Arifan Hr <rahmadarifanhr@192.168.1.4>
2021-01-05 11:13:25 +0700

    first commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.btn).setOnClickListener(v -> startActivity(new Intent(MainActivity.this, MainActivity2.class)));
+
+    }
+}
